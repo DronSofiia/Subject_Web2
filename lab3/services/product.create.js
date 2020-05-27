8f427ba13b017f93cbcf837a4b5903d90ead239a
leofun01 <leofun01@gmail.com>
2020-05-27 15:28:54 +0300

    Add services/product.create.js

---
+++ lab3/services/product.create.js
@@ -0,0 +1,24 @@
+﻿const Product = require('./../models/product')
+
+/**
+ * @param {Object} data
+ */
+module.exports = function (data) {
+  const product = new Product({
+    sku: data.sku,
+    name: data.name,
+    price: data.price,
+    weight: data.weight,
+    volume: data.volume
+  })
+
+  return new Promise((resolve, reject) => {
+    product.save(function (err, createdProduct) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(createdProduct)
+      }
+    })
+  })
+}
